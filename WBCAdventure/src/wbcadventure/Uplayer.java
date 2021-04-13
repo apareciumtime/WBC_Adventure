641f2cdf97c0749223372157ad48220670a707c5
APARECIUM <hikarugrize@gmail.com>
2021-04-13 16:31:40 +0700

    commit Uplayer : add method public wbcadventure.SkillBar getSkillBarPanel(){}

--- WBCAdventure/src/wbcadventure/Uplayer.java
+++ WBCAdventure/src/wbcadventure/Uplayer.java
@@ -6,13 +6,21 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.border.Border;
+
+/**
+ * The Panel Receive and Output all function on Up-Layered
+ * @author Developers
+ */
 public class Uplayer extends JPanel{
     
     private SkillBar skillBarPanel = new SkillBar();
     private ProgressBar hpBarPanel = new ProgressBar();
     private Border border = BorderFactory.createLineBorder(Color.gray,1);
     
-    
+    /**
+     * Constructor create The Up-layer Panel
+     * Contain 1.Skill Bar Panel 2.HP Bar Panel 3.Progression Bar Panel
+     */
     public Uplayer(){
         skillBarPanel.setBounds(50,800, 616, 154);
         skillBarPanel.setOpaque(false);
@@ -22,8 +30,14 @@ public class Uplayer extends JPanel{
         
         this.setLayout(null);
         this.add(skillBarPanel);
-        this.add(hpBarPanel);
-        
-        
+        this.add(hpBarPanel);    
+    }
+    
+    /**
+     * Method return Skill Bar Panel from SkillBar Class
+     * @return Panel from SkillBar Class as SkillBar Class Object
+     */
+    public wbcadventure.SkillBar getSkillBarPanel(){
+        return skillBarPanel;
     }
 }
