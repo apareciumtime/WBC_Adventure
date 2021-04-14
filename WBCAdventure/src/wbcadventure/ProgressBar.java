0e68ea66b071b590643b970f400fde1dde1088c2
APARECIUM <hikarugrize@gmail.com>
2021-04-14 21:56:51 +0700

    commit ProgressBar class : setWBCHPBar, setEnemyHPBar, setBossHPBar

--- WBCAdventure/src/wbcadventure/ProgressBar.java
+++ WBCAdventure/src/wbcadventure/ProgressBar.java
@@ -9,52 +9,132 @@ import javax.swing.JPanel;
 import javax.swing.border.Border;
 public class ProgressBar extends JPanel{
 
-    WBC wbcCharacter = new WBC();
-    JProgressBar hpBarWBC = new JProgressBar();
-    JLayeredPane layer = new JLayeredPane();
+    
+    private JLayeredPane layer = new JLayeredPane();
     
     private Border border = BorderFactory.createLineBorder(Color.gray,1);
-
+    
+    private WBC wbcCharacter = new WBC();
+    private JLabel wbcHPBarLabel        = new JLabel();
+    private JLabel wbcProgressBarLabel  = new JLabel();
+    private JLabel wbcHPBarIcon         = new JLabel();
+    private JProgressBar hpBarWBC = new JProgressBar();
+    
+    private NormalEnemy enemyCharacter = new NormalEnemy(wbcCharacter);
+    private JLabel enemyHPBarLabel          = new JLabel();
+    private JLabel enemyProgressBarLabel    = new JLabel();
+    private JLabel enemyHPBarIcon           = new JLabel();
+    private JProgressBar hpBarEnemy = new JProgressBar();
+    
+    private BossEnemy bossCharacter = new BossEnemy(wbcCharacter);
+    private JLabel bossHPBarLabel       = new JLabel();
+    private JLabel bossProgressBarLabel  = new JLabel();
+    private JLabel bossHPBarIcon        = new JLabel();
+    private JProgressBar hpBarBoss = new JProgressBar();
     
     public ProgressBar(){
-        
-//        
-//        JLabel hpBarWBCLabel = new JLabel();
-//        hpBarWBCLabel.add(hpBarWBC);
-//        hpBarWBCLabel.setBounds(0, 0, 1920, 100);
-
-        JLabel HPBarWBCLabel = new JLabel();
-        JLabel progressBarWBC = new JLabel();
-        JLabel HPBarWBCImg = new JLabel();
-        
-        HPBarWBCImg.setIcon(new ImageIcon("src/source/uplayer/HPBar/HPBarWBC.png"));
-        HPBarWBCImg.setOpaque(false);
-        HPBarWBCImg.setBounds(0, 0, 790, 75);
-        HPBarWBCImg.setBorder(border);
-        
-        
-        hpBarWBC.setValue(wbcCharacter.getHPcontrol().getMaxHP());
-//        hpBarWBC.setStringPainted(true);
-        hpBarWBC.setBounds(0,0,565, 42);
-        hpBarWBC.setForeground(new Color(0x6fc998));
-        hpBarWBC.setBackground(new Color(0xde9898));
-        hpBarWBC.setValue(50);
-        progressBarWBC.add(hpBarWBC);
-        progressBarWBC.setBounds(210,15,565,42);
-        layer.add(progressBarWBC,Integer.valueOf(0));
-
-        
-//        this.add(hpBarWBCLabel);
-
-        layer.add(HPBarWBCImg,Integer.valueOf(1));
-        layer.setBounds(0, 0, 1920 , 75);
-        layer.setBorder(border);
-        
-        layer.setVisible(true);
         this.add(layer);
         this.setLayout(null);
         this.setBounds(0, 0, 1920, 100);
-        this.setBorder(border);
+//        this.setBorder(border);
     }
-
+    
+    public void setWBCHPBar(){
+        wbcHPBarIcon.setIcon(new ImageIcon("src/source/uplayer/HPBar/HPBarWBC.png"));
+        wbcHPBarIcon.setOpaque(false);
+        wbcHPBarIcon.setBounds(0, 0, 790, 75);
+//        wbcHPBarIcon.setBorder(border);
+        
+        hpBarWBC.setMaximum(wbcCharacter.getHPcontrol().getMaxHP());
+        hpBarWBC.setStringPainted(true);
+        hpBarWBC.setString(""+wbcCharacter.getHPcontrol().getHP());
+        
+        hpBarWBC.setBounds(0,0,565, 42);
+        hpBarWBC.setForeground(new Color(0x6fc998));
+        hpBarWBC.setBackground(new Color(0xde9898));
+        hpBarWBC.setValue(wbcCharacter.getHPcontrol().getHP());
+        
+        wbcProgressBarLabel.add(hpBarWBC);
+        wbcProgressBarLabel.setBounds(210,15,565,42);
+        
+        layer.add(wbcProgressBarLabel, Integer.valueOf(0));
+        layer.add(wbcHPBarIcon,Integer.valueOf(1));
+        layer.setBounds(0, 0, 1920 , 75);
+//        layer.setBorder(border);
+    }
+    
+    public void setEnemyHPBar(){
+        enemyHPBarIcon.setIcon(new ImageIcon("src/source/uplayer/HPBar/HPBarEnemy.png"));
+        enemyHPBarIcon.setOpaque(false);
+        enemyHPBarIcon.setBounds(1110, 0, 790, 75);
+//        enemyHPBarIcon.setBorder(border);
+        
+        hpBarEnemy.setMaximum(enemyCharacter.getHPcontrol().getMaxHP());
+        hpBarEnemy.setStringPainted(true);
+        hpBarEnemy.setString(""+(enemyCharacter.getHPcontrol().getMaxHP()-enemyCharacter.getHPcontrol().getHP()));
+        hpBarEnemy.setValue(0);
+        
+        hpBarEnemy.setBounds(0,0,565, 42);
+        hpBarEnemy.setForeground(new Color(0xde9898));
+        hpBarEnemy.setBackground(new Color(0x6fc998));
+        
+        enemyProgressBarLabel.add(hpBarEnemy);
+        enemyProgressBarLabel.setBounds(1129,15,565,42);
+        
+        layer.add(enemyProgressBarLabel, Integer.valueOf(0));
+        layer.add(enemyHPBarIcon,Integer.valueOf(1));
+        layer.setBounds(0, 0, 1920 , 75);
+//        layer.setBorder(border);        
+        
+    }
+    
+    public void setBossHPBar(){
+        bossHPBarIcon.setIcon(new ImageIcon("src/source/uplayer/HPBar/HPBarEnemy.png"));
+        bossHPBarIcon.setOpaque(false);
+        bossHPBarIcon.setBounds(1110, 0, 790, 75);
+//        bossHPBarIcon.setBorder(border);
+        
+        hpBarBoss.setMaximum(bossCharacter.getHPcontrol().getMaxHP());
+        hpBarBoss.setStringPainted(true);
+        hpBarBoss.setString(""+(bossCharacter.getHPcontrol().getMaxHP()-bossCharacter.getHPcontrol().getHP()));
+        hpBarBoss.setValue(0);
+        
+        hpBarBoss.setBounds(0,0,565, 42);
+        hpBarBoss.setForeground(new Color(0xde9898));
+        hpBarBoss.setBackground(new Color(0x6fc998));
+        
+        bossProgressBarLabel.add(hpBarBoss);
+        bossProgressBarLabel.setBounds(1129,15,565,42);
+        
+        layer.add(bossProgressBarLabel, Integer.valueOf(0));
+        layer.add(bossHPBarIcon,Integer.valueOf(1));
+        layer.setBounds(0, 0, 1920 , 75);
+//        layer.setBorder(border);        
+    }
+    
+    public wbcadventure.Character getWBCCharacter(){
+        return wbcCharacter;
+    }
+    
+    public wbcadventure.Character getEnemyCharacter(){
+        return enemyCharacter;
+    }
+    
+    public wbcadventure.Character getBossCharacter(){
+        return bossCharacter;
+    }
+    
+    public JProgressBar getHPBarWBC(){
+        return hpBarWBC;
+    }
+    
+    public JProgressBar getHPBarEnemy(){
+        return hpBarEnemy;
+    }
+    
+    public JProgressBar getHPBarBoss(){
+        return hpBarBoss;
+    }
+    
+    
 }
